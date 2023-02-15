43855be730e9753df435eb264a68302b2571f554
Jasiostwor <janekhorodecki@gmail.com>
2023-02-15 09:37:50 +0100

    Bot activity

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,13 @@
-const { Events } = require('discord.js');
+const { Events, ActivityType } = require('discord.js');
 const logger = require('./../modules/logger');
 
 module.exports = {
     name: Events.ClientReady,
 	once: true,
 	execute(client) {
+		client.user.setActivity("Banning guild members", {type: ActivityType.Competing});
+		client.user.setStatus('dnd');
+
 		logger.info(`Ready! Logged in as ${client.user.tag}`);
-		// console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
